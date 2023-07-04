43e2a1344af367852f762fbe8611e5f452721ee4
lee <leejiliang@126.com>
2023-07-04 15:49:37 +0800

    feat: 项目初始化, 引入boot依赖, 使用版本, 2.7.13

---
+++ src/main/java/com/uk/bootintegrationall/BootIntegrationAllApplication.java
@@ -0,0 +1,13 @@
+package com.uk.bootintegrationall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootIntegrationAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootIntegrationAllApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uk/bootintegrationall/BootIntegrationAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uk.bootintegrationall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootIntegrationAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
